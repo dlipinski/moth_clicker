a5ae000f019b579cda4ad6eb644bf076b21961cf
dlipinski <dawid.lipinski3@gmail.com>
2018-10-18 22:24:15 +0200

    2

--- script.js
+++ script.js
@@ -1,43 +1,71 @@
+/* jshint esversion: 6, browser: true */
+/* globals $, window*/
 
-$(()=>{
-    const bulb_head = $("#head");
-    const light_control = $("#light_control");
-    const light_value = $("#light_value");
-    let window_height = window.innerHeight;
-    let window_width = window.innerWidth - 100;
-    let horizontalCenter = Math.floor(window.innerWidth/2)-100;
-    let verticalCener = Math.floor(window.innerHeight/2)-100;
-    var lightess = 110;
-    var moth_counter = 0;
-    if(!moth_counter){
-        moth_counter = 0;
+const getCookie = (cname) => {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
     }
-    var moth_multiplier = 1;
-    const change_lightness = (light_volume) => {
-        let r = (light_volume/1000) * 255;
-        let g = r;
-        let b = (light_volume/1000) * 130;
-        let light = (light_volume/1000) * 30;
-        bulb_head.css("background","rgb("+r+","+g+","+b+")")
-        bulb_head.css("box-shadow","0px 0px 100px "+light+"px rgb("+r+", "+g+", "+b+")")
-    };
-    change_lightness(lightess);
+    return "";
+};
+const setCookie = (cname, cvalue, exdays) => {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    var expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+};
+const random_boolean = () =>{
+    return Math.random() >= 0.5;
+};
+let game = {
+    moth_counter: 0,
+    moth_counter_total: 0,
+    moth_multiplier: 1,
+    click_multiplier: 1,
+    bulb_level: 1
+};
+let lightess = 110;
+let horizontalCenter = Math.floor(window.innerWidth/2)-100;
+let verticalCener = Math.floor(window.innerHeight/2)-100;
+const set_game_from_cookies = () => {
+    game.moth_counter = getCookie("moth_counter") === "" ? 0 :  getCookie("moth_counter");
+    game.moth_counter_total = getCookie("moth_counter_total") === "" ? game.moth_counter :  getCookie("moth_counter_total");
+   //game.moth_multiplier = getCookie("moth_multiplier") === "" ? 1 :  getCookie("moth_multiplier");
+};
+const print_moths = () => {
+    $("#wealth").html("Moths: "+game.moth_counter);
+};
+const change_lightness = (light_volume) => {
+    let r = (light_volume/1000) * 255;
+    let g = r;
+    let b = (light_volume/1000) * 130;
+    let light = (light_volume/1000) * 30;
+    $("#head").css("background","rgb("+r+","+g+","+b+")")
+    $("#head").css("box-shadow","0px 0px 100px "+light+"px rgb("+r+", "+g+", "+b+")")
+};
+const set_intervals = () => {
+    /* Darken light in time */
     const light_interval = setInterval(()=>{
         if(lightess>100){
             lightess-=10;
             change_lightness(lightess);
-            light_value.html(lightess);
         } else{
-            bulb_head.css("rgb(71, 71, 71)")
-            bulb_head.css("box-shadow","0px 0px 0px 0px")
+            $("#head").css("background","rgb(71, 71, 71)");
+            $("#head").css("box-shadow","0px 0px 0px 0px");
         }
     },100);
-    const random_boolean = () =>{
-        return Math.random() >= 0.5;
-    };
+    /* Generate moths */
     const moth_interval = setInterval(()=>{
         if(lightess>Math.floor((Math.random() * 1000) + 100)){
-            for(let i=0;i<moth_multiplier;i++){
+            for(let i=0;i<1;i++){
                 let left=0;
                 let top = 0;
                 let is_side = random_boolean();
@@ -46,17 +74,17 @@ $(()=>{
                     if(is_left){
                         left = -100;
                     } else {
-                        left = window_width + 100;
+                        left =  window.innerWidth + 100;
                     }
-                    top = Math.floor((Math.random() * window_height) + 1);
+                    top = Math.floor((Math.random() * window.innerHeight) + 1);
                 } else {
                     let is_top = random_boolean();
                     if(is_top){
                         top = -100;
                     } else {
-                        top = window_height + 100;
+                        top =  window.innerWidth + 100;
                     }
-                    left = Math.floor((Math.random() * window_width) + 1);
+                    left = Math.floor((Math.random() *  window.innerWidth) + 1);
                 }
                 $("<div class='moth' style='top:"+top+"px;left:"+left+"px;'></div>")
                 .appendTo($('body'))
@@ -64,32 +92,56 @@ $(()=>{
                     'top': verticalCener,
                     'left': horizontalCenter
                     
-                }, 1000, () => { 
-                    moth_counter++;
-                    $('#wealth').html("Moths: "+moth_counter);
+                }, 1000, function () { 
                     $(this).remove();
+                    game.moth_counter++;
+                    game.moth_counter_total++;
+                    document.cookie = "moth_counter="+game.moth_counter;
+                    document.cookie = "moth_counter_total="+game.moth_counter_total;
+                    print_moths();
                 });
             }
         }
-        },1000);
-    bulb_head.click(()=>{
-        if(lightess+50<1000){
+        },1000/game.moth_multiplier);
+};
+
+const addListeners = () => {
+    $("#head").click( () => {
+        console.log("clicked");
+        /*if(lightess+50<1000){
             lightess+=50;
             change_lightness(lightess);
-            light_value.html(lightess);
-        }
-
+        }*/
     });
-    $(window).resize(()=>{
-        window_height = window.innerHeight;
-        window_width = window.innerWidth - 100;
+};
+
+$(()=>{
+    set_game_from_cookies();
+    print_moths();
+    set_intervals();
+    change_lightness(lightess);
+    addListeners();
+    
+    /*
+    $("#head").on("click", function(){
+        console.log("clicked_on");
+    });
+    
+    $('#multiplier').click( () => {
+        game.moth_multiplier++;
+        $('multiplier_amount').html("Multiplier: "+ game.moth_multiplier);
+    });
+
+    $(window).resize( () => {
         horizontalCenter = Math.floor(window.innerWidth/2)-100;
         verticalCener = Math.floor(window.innerHeight/2)-100;
     });
 
-   setInterval(()=>{
-    bulb_head.click();
+   setInterval( () => {
+    $("#head").click();
    },100);
+
+  */
 
   
 });
